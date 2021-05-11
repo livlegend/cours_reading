c9befff44c4ec0a2dd7e73c2896cfeafaca34e6c
livlegend <livsenyo@gmail.com>
2021-05-11 14:24:41 +0000

    nothing

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -5,6 +5,7 @@ import 'package:bookshelf_app/data/data.dart';
 import 'package:bookshelf_app/models/book_model.dart';
 import 'package:bookshelf_app/models/single_book_model.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'dart:ui' as ui;
 import 'package:bookshelf_app/resource/widgets.dart';
 
 String slectedCategorie = "All";
@@ -73,9 +74,38 @@ class _MyHomePageState extends State<MyHomePage> {
                 SizedBox(
                   height: 16,
                 ),
-                Image.asset(
-                  "assets/banner.png",
-                  fit: BoxFit.fitWidth,
+
+                Container(
+                  child:Stack(
+                    children: <Widget>[
+                      Container(
+                        height:150,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(24),
+                          gradient: LinearGradient(
+                            colors: [Colors.pink, Colors.red],
+                            begin: Alignment.topLeft,
+                            end:Alignment.bottomRight
+                          ),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.red,
+                              blurRadius: 12,
+                              offset: Offset(0,6),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Positioned(
+                        right: 0,
+                        bottom: 0,
+                        top: 0,
+                        child:CustomPaint(
+                          painter: CustomCardShapePainter(24, Colors.pink, Colors.red),
+                        ),
+                      ),
+                    ],
+                  )
                 ),
                 SizedBox(
                   height: 20,
@@ -272,4 +302,44 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
     );
   }
+}
+
+class CustomCardShapePainter  extends CustomPainter{
+  final double radius;
+  final Color startColor;
+  final Color endColor;
+  CustomCardShapePainter(this.radius, this.startColor, this.endColor);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    var radius = 24.0;
+
+    var paint = Paint();
+    paint.shader = ui.Gradient.linear(
+        Offset(0, 0), Offset(size.width, size.height), [
+      HSLColor.fromColor(startColor).withLightness(0.8).toColor(),
+      endColor
+    ]);
+
+    var path = Path()
+      ..moveTo(0, size.height)
+      ..lineTo(size.width - radius, size.height)
+      ..quadraticBezierTo(
+          size.width, size.height, size.width, size.height - radius)
+      ..lineTo(size.width, radius)
+      ..quadraticBezierTo(size.width, 0, size.width - radius, 0)
+      ..lineTo(size.width - 1.5 * radius, 0)
+      ..quadraticBezierTo(-radius, 2 * radius, 0, size.height)
+      ..close();
+
+    canvas.drawPath(path, paint);
+    // TODO: implement paint
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    // TODO: implement shouldRepaint
+    return true;
+  }
+
 }
